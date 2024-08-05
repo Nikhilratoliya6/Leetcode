a3a62f673632ba55e8fde5be79b6e066e58720db
NIKHIL RATOLIYA <132206949+Nikhilratoliya6@users.noreply.github.com>
2024-08-05 10:38:58 +0530

    Time: 7 ms (55.29%), Space: 44.7 MB (6.52%) - LeetHub

---
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> h = new HashMap<>();
+        
+        // Count the occurrences of each string in the array
+        for (String num : arr) {
+            h.put(num, h.getOrDefault(num, 0) + 1);
+        }
+    
+        for (String num : arr) {
+            if (h.get(num) == 1) {
+                k--;
+                if (k == 0) {
+                    return num;
+                }
+            }
+        }
+        return "";
+    }
+}
